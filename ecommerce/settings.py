d1f898a554fdb44f6608b0c56ef82a00aa2d2ec0
Brook Kynaston <brookkynaston@live.fr>
2019-03-13 18:53:35 +0000

    Started connection to Heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!8v(^oilgqkn)pc#^lxnex8-7gj2ol7&^s9h-3pi@=eh^ie#d4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,12 +83,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#if "DATABASE_URL" in os.environ:
+#    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+#else:
+#   print("Database URL not found. Using SQLite instead")
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#    }
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault("STRIPE_PUBLISHABLE","pk_test_DnoBniSp1Ga40gmG3Y58a6C1")
 os.environ.setdefault("STRIPE_SECRET","sk_test_ALjlv06vHZahJhrWePNByxzl")
+os.environ.setdefault("DATABASE_URL", "postgres://howbguxsqiffqk:8dd6b7e6192aed09a0985d8f75bc65c7aea415a547549c00a42d9daffb8bb81f@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/d242h4blvgj38u")
+os.environ.setdefault("SECRET_KEY", "!8v(^oilgqkn)pc#^lxnex8-7gj2ol7&^s9h-3pi@=eh^ie#d4")
