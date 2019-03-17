3e97146d26bf16ff5cd168b81bbc93e1adc86d23
Brook Kynaston <brookkynaston@live.fr>
2019-03-17 18:52:13 +0000

    still testing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -115,7 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'accounts.backends.CaseInsensitiveAuth']
+    'accounts.backends.CaseInsensitiveAuth'
+    ]
 
 
 # Internationalization
@@ -152,7 +154,7 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIAFILES_LOCATION = 'media'
